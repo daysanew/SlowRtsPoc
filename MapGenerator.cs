0d5b91bcbcd311dbacbab675f24f4e28e2af721a
Charlie Leverett <Charlie.Leverett@vu.com>
2019-08-29 21:32:01 -0500

    Initial checkin

---
+++ MapGenerator.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+
+namespace RTSGamePoc
+{
+    public class MapGenerator
+    {
+        private readonly int _mapBoundary;
+        private int[,] _map;
+        private int _numberOfPlayers;
+        private readonly int _regions;
+        private readonly int _averageNumberTowns = 4;
+        private readonly int _averageRegionSize = 20;
+
+        public MapGenerator(int mapSize, int numberOfPlayers)
+        {
+            _mapBoundary = mapSize;
+            _numberOfPlayers = numberOfPlayers;
+            _map = new int[mapSize, mapSize];
+        }
+        private List<ILocation> GenerateLocations()
+        {
+            var random = new Random();
+            var numberOfRegions = random.Next(_numberOfPlayers, _numberOfPlayers * 2);
+            Coordinate currentCoordinates = new Coordinate
+            {
+                X = 0,
+                Y = 0
+            };
+
+            for (var i = 0; i == numberOfRegions; i++)
+            {
+
+            }
+
+            return new List<ILocation>();
+        }
+
+        private List<ILocation> GenerateRegion(Coordinate startBoundary)
+        {
+            var random = new Random();
+            var regionSize = random.Next(_averageRegionSize, _averageRegionSize * 2);
+            Coordinate endBoundary = new Coordinate
+            {
+                X = startBoundary.X + regionSize,
+                Y = startBoundary.Y + regionSize
+            };
+
+            var castle = new Castle();
+
+            var numberOfTowns = random.Next(_averageNumberTowns / 2, _averageNumberTowns * 2);
+            return new List<ILocation>();
+        }
+    }
+
+    public interface ILocation
+    {
+        Coordinate coordinate { get; set; }
+        List<ILocation> location { get; set; }
+    }
+
+    public class Castle : ILocation
+    {
+        public Coordinate coordinate { get; set; }
+        public List<ILocation> location { get; set; }
+    }
+
+    public class Town : ILocation
+    {
+        public Coordinate coordinate { get; set; }
+        public List<ILocation> location { get; set; }
+    }
+
+    public class Coordinate
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
